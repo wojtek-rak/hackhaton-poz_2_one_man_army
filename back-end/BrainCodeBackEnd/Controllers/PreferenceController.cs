592885c832b77c1ed79f9cac87f81dbfe47f2728
Wojciech Rak <rak.wojtek@wp.pl>
2019-04-13 09:09:49 +0200

    add preference endpoint

--- back-end/BrainCodeBackEnd/Controllers/PreferenceController.cs
+++ back-end/BrainCodeBackEnd/Controllers/PreferenceController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using BrainCodeBackEnd.Models;
@@ -9,6 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BrainCodeBackEnd.Controllers
 {
+    [Route("api/[controller]")]
     public class PreferenceController : Controller
     {
 
@@ -18,8 +20,8 @@ namespace BrainCodeBackEnd.Controllers
             _delivery = delivery;
         }
         // GET: /<controller>/
-        [HttpGet("{price}/{category}")]
-        public string GetPreferences(int price, string category)
+        [HttpGet("{price}/{category}/{filter}")]
+        public string GetPreferences(int price, string category, string filter)
         {
             string paczkomat = "paczkomat";
             string odbior = "odbior";
@@ -30,13 +32,31 @@ namespace BrainCodeBackEnd.Controllers
             List<String> paczkCat = new List<string>{ "Moda", "Uroda" };
             List<String> odbiorCat = new List<string> { "Motoryzacja" };
 
+            var filters = filter.Split(",");
+
+            float dl;
+            float.TryParse(filters[0], NumberStyles.Any, CultureInfo.InvariantCulture, out dl);
+            float sr;
+            float.TryParse(filters[1], NumberStyles.Any, CultureInfo.InvariantCulture, out sr);
+
+
             //to chce od wojtka 
-            List<float> distance = new List<float>() { 1000, 10, 50, 150, 200, 300 };
-            float minDistance = distance.Min();
+
+            var maxFilterDistance = 0.1f;
+
+            var filteredList = _delivery.deliveryList
+                .Where(x => Math.Abs(x.Dlugosc - dl) < maxFilterDistance && Math.Abs(x.Szerokosc - sr) < maxFilterDistance).ToList();
+
+            var distance = filteredList.Select(x =>
+                Math.Sqrt((x.Szerokosc - sr) * (x.Szerokosc - sr) + (x.Dlugosc - dl) * (x.Dlugosc - dl))).ToList();
+
+            //List<float> distance = new List<float>() { 1000, 10, 50, 150, 200, 300 };
+            var minDistance = distance.Min();
 
             List<int> lastTen = new List<int>(){ 8, 0, 10, 15, 0, 0, 25 };
             float avgDelivery = (float)lastTen.Average();
 
+            var minDist = 0.05d;
 
             if (homeCat.Contains(category))
             {
@@ -46,7 +66,7 @@ namespace BrainCodeBackEnd.Controllers
                 }
                 else
                 {
-                    if (minDistance < 500)
+                    if (minDistance < minDist)
                     {
                         result = paczkomat;
                     }
@@ -71,7 +91,7 @@ namespace BrainCodeBackEnd.Controllers
                 }
                 else
                 {
-                    if (minDistance < 500)
+                    if (minDistance < minDist)
                     {
                         result = paczkomat;
                     }
