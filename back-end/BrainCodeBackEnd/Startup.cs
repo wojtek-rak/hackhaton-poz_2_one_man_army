ea39efd4e669f87d85f19d4e75898fa1f9647247
Wojciech Rak <rak.wojtek@wp.pl>
2019-04-13 02:31:06 +0200

    add cors

--- back-end/BrainCodeBackEnd/Startup.cs
+++ back-end/BrainCodeBackEnd/Startup.cs
@@ -25,6 +25,14 @@ namespace BrainCodeBackEnd
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    opt => opt.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+            });
+
             var reader = new CSVReader();
             Delivery delivery = new Delivery();
             delivery = reader.GetCSV();
